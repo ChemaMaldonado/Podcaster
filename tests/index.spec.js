28a408bfe4afd0c8d71d2917f3d33870ecf4c656
Chema Maldonado <comercial@MacBook-Air-de-comercial.local>
2022-10-25 00:28:16 +0200

    Add one test e2e to check basic behaviour

---
+++ tests/index.spec.js
@@ -0,0 +1,19 @@
+import { expect, test } from '@playwright/test'
+
+test('test basic functionallity', async ({ page }) => {
+  await page.goto('http://localhost:5173/')
+
+  await page.getByPlaceholder('Search by artist or title...').click()
+  await page.getByPlaceholder('Search by artist or title...').fill('No jumper')
+  await page.getByRole('link', { name: 'No Jumper Author: No Jumper' }).click()
+  await expect(page).toHaveURL('http://localhost:5173/podcast/1001659715')
+
+  await page.getByRole('cell', { name: 'Drummer Boy on Pomona Upbringing, Growing Out of Gangbanging, Thuggin on GTA & More' }).click()
+  await expect(page).toHaveURL('http://localhost:5173/podcast/1001659715/episode/1000583551694')
+
+  await page.getByRole('link', { name: 'No Jumper Author: No Jumper' }).click()
+  await expect(page).toHaveURL('http://localhost:5173/podcast/1001659715')
+
+  await page.getByRole('link', { name: 'PodCaster' }).click()
+  await expect(page).toHaveURL('http://localhost:5173/')
+})
